c26407f7c59f0014b59767b9c4cf64c4cc6ffaee
Jordan Ren <51218151+Jordan-Ren@users.noreply.github.com>
2019-08-09 11:05:10 -0400

    Basic website with forms that connect to a DB

---
+++ MVCTest/MVCTest/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MVCTest.Models;
+using RazorPagesMovie.Models;
+
+namespace MVCTest.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MVCTest.Models.MVCTestContext _context;
+
+        public IndexModel(MVCTest.Models.MVCTestContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
